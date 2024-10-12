7ba40fd1e156c65545cba9d095a536bfa9ae72ca
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-10-12 14:50:47 +0530

    updating file

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,19 @@
-import React from "react";
-
 export default function Footer() {
   return (
     <footer className=" text-black py-4">
       <div className="container mx-auto text-center">
         <p className="text-sm">
           &copy; {new Date().getFullYear()} Designed by{" "}
-          <a href="https://anshdev.vercel.app/" 
-          className="hover:underline text-blue-500
+          <a
+            href="https://anshdev.vercel.app/"
+            className="hover:underline text-blue-500
+          
 
-          ">
-             Ansh
+          "
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ansh
           </a>
           . All rights reserved.
         </p>
